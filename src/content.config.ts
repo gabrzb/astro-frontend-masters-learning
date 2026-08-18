959f8e4bcfc3971bddd09317d2b940c890a5d5d2
gabrzb <gabrielzbz.gz@gmail.com>
2026-08-18 16:38:19 -0300

    chore: added Blog page with content

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// src/content.config.ts
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+  loader: glob({
+    base: './src/content/blog',
+    pattern: '**/*.{md,mdx}',
+  }),
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    date: z.coerce.date(),
+    description: z.string().max(200),
+  }),
+});
+
+export const collections = { blog };
