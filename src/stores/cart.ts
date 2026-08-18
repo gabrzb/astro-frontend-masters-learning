8c563cf21a13ab148b11a672d07311cade8c0f0d
gabrzb <gabrielzbz.gz@gmail.com>
2026-08-18 15:07:27 -0300

    feat: added Sharing State with nanostores

---
+++ src/stores/cart.ts
@@ -0,0 +1,32 @@
+import { computed, map } from 'nanostores';
+
+export const $cart = map<Record<number, CartItem>>({});
+
+export function addItemToCart(item: CartItem) {
+  const cartItem = $cart.get()[item.id];
+  const quantity = cartItem ? cartItem.quantity : 0;
+
+  $cart.setKey(item.id, {
+    item,
+    quantity: quantity + 1,
+  });
+}
+
+export function removeItemFromCart(item: CartItem) { 
+  // @ts-ignore
+  $cart.setKey(item.id, undefined);
+}
+
+export const subTotal = computed($cart, (entries) => {
+  let subTotal = 0;
+  
+  Object.values(entries).forEach((entry) => {
+    if (!entry) {
+      return;
+    }
+    
+    subTotal += entry.item.price * entry.quantity;
+  });
+  
+  return subTotal;
+});
